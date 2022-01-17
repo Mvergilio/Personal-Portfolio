d8d18cf0b51d9aa984f7563043b249eb31e99b4d
Mvergilio <mvergiliodesouza@gmail.com>
2022-01-17 12:46:05 -0300

    finished the smoth behavior

---
+++ Portfolio/main.js
@@ -0,0 +1,25 @@
+// selection the elements
+const LINKS_TO_SECTION = document.querySelectorAll('.navlink');
+const NAV_BAR = document.getElementById('navbar');
+
+// event listeners
+LINKS_TO_SECTION.forEach((link)=>{
+    link.addEventListener('click',(event)=>{
+        // prevent default event bahavior
+        event.preventDefault();
+        
+        let href = link.getAttribute('href').slice(1);
+        let section = document.getElementById(href);
+        let navHeight = NAV_BAR.getBoundingClientRect().height;
+        let yPosition= section.offsetTop - navHeight;
+    
+ //scroll behavior    
+    window.scrollTo({
+        top: yPosition,
+        left: 0,
+        behavior: 'smooth'
+      });
+    })
+   
+
+})
