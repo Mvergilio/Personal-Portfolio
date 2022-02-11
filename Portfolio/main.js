fe684a36dee0dec84d45a666c33b614b08c14091
Mvergilio <mvergiliodesouza@gmail.com>
2022-02-11 12:51:16 -0300

    Working on the language feature

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -3,7 +3,41 @@ const LINKS_TO_SECTION = document.querySelectorAll(".navlink");
 const NAV_BAR = document.getElementById("navbar");
 const TIME = document.querySelector(".time");
 const LINKS_CONTAINER = document.querySelector(".links");
+const MENU_LINKS = document.querySelector(".menu-links")
+const HIDEN_MAIN = document.querySelector(".hidden");
+const LANG_BTNS = document.querySelectorAll(".leng");
 // event listeners
+
+// languages event
+let langCheck = false;
+window.sessionStorage.setItem("check", langCheck);
+console.log(sessionStorage)
+  LANG_BTNS.forEach((element)=>{
+  element.addEventListener("click",(e)=>{
+  // let textContent = this.Target.textContent;
+  if(element.classList.contains("portuguese")){
+    alert("It is working")
+    console.log("IT IS WORKING")
+  setItem();
+  }
+  
+})
+})
+function setItem(){
+  
+  langCheck = true;
+  window.sessionStorage.setItem("check", langCheck)
+  let sessionStorage = window.sessionStorage
+  return sessionStorage
+}
+async function display(){
+
+        let storage = setItem();
+        console.log(storage)
+      }
+
+display();
+
 window.addEventListener("scroll", (e) => {
   const TOP_POSITION_OF_THE_PAGE = LINKS_CONTAINER.offsetTop;
   let pageYOffSet = window.scrollY;
@@ -25,7 +59,7 @@ LINKS_TO_SECTION.forEach((link) => {
     let href = link.getAttribute("href").slice(1);
     let section = document.getElementById(href);
     if (section == null) {
-      this.href = "./english.html";
+      this.href = "./PT-EN/english.html";
       window.location = this.href;
     } else {
       let navHeight = NAV_BAR.getBoundingClientRect().height;
